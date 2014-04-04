ffdde2301a84ad6783eedd3ac1540bf4979c265b
horike <horike@digitalcube.jp>
2014-04-05 03:20:38 +0900

    1.2.7

--- simple-related-posts.php
+++ simple-related-posts.php
@@ -4,7 +4,7 @@ Plugin Name: WP Simple Related Posts
 Plugin URI: http://www.kakunin-pl.us/
 Description: Display Related Posts. Very Simple.
 Author: horike takahiro
-Version: 1.2.6
+Version: 1.2.7
 Author URI: http://www.kakunin-pl.us/
 
 
