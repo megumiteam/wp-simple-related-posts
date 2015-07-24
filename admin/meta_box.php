8222003d2bd0efa3f7523edaefb7329759edb14b
horike <horike@digitalcube.jp>
2015-07-24 16:26:36 +0900

    set auto save

--- admin/meta_box.php
+++ admin/meta_box.php
@@ -20,6 +20,10 @@ class Simple_Related_Posts_Admin_Meta_Box {
 	}
 	
 	public function save_post($post_id) {
+		if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+			return;
+		}
+
 		if ( isset($_POST['simple_related_posts']) && is_array($_POST['simple_related_posts'])) {
 			update_post_meta( $post_id, 'simple_related_posts', $_POST['simple_related_posts'] );
 		}
